719b216d6f48342e153320ba53e0e4526afda9ed
nevil <38464608+nevil-18@users.noreply.github.com>
2022-02-16 15:17:38 +0530

    Add files via upload

---
+++ chatify/src/App.js
@@ -0,0 +1,122 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth'; 
+
+import { useAuthState} from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBUHVdgl82AWQ3rC30nUQJDaQX5L4iVocs",
+  authDomain: "chatify-db.firebaseapp.com",
+  projectId: "chatify-db",
+  storageBucket: "chatify-db.appspot.com",
+  messagingSenderId: "80198346225",
+  appId: "1:80198346225:web:c9f710f0862c68ffbc1050",
+  measurementId: "G-XDF9FBK48M"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+                                          
+      <section>                             
+       {user ? <ChatRoom /> : <SignIn />}   
+      </section>
+    </div>
+  );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://drive.google.com/file/d/1cXQT7Lyap0GWjcfgoAM3uiMkO8e86OsV/view?usp=sharing' } alt='' />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
+
+export default App;
